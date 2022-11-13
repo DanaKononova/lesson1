88ea572bdb9fb270fd3629a96da88d7f9e693e31
DanaKononova <dana.kononova@mail.ru>
2022-11-13 18:37:01 +0300

    commit task1

--- app/src/main/java/com/example/lesson1/Homework.java
+++ app/src/main/java/com/example/lesson1/Homework.java
@@ -1,7 +1,20 @@
 package com.example.lesson1;
 
-public class Homework {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Homework {
+    public static void task1() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int n = scan.nextInt();
+        if (n % 2 == 0) {
+            System.out.println(n+" - чётное");
+        } else {
+            System.out.println(n+" - нечётное");
+        }
+    }
+
+    public static void main(String[] args){
+        task1();
     }
 }
