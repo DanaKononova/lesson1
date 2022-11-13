f89ec54155e6e77185da291a7d3f4f68eab3e4ba
DanaKononova <dana.kononova@mail.ru>
2022-11-13 18:46:44 +0300

    commit task2

--- app/src/main/java/com/example/lesson1/Homework.java
+++ app/src/main/java/com/example/lesson1/Homework.java
@@ -14,7 +14,29 @@ public class Homework {
         }
     }
 
+    public static void task2() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите три числа:");
+        float a = scan.nextFloat();
+        float aModule = a < 0 ? (-1) * a : a;
+        float b = scan.nextFloat();
+        float bModule = b < 0 ? (-1) * b : b;
+        float c = scan.nextFloat();
+        float cModule = c < 0 ? (-1) * c : c;
+        System.out.println("Меньшее по модулю число: ");
+        if (aModule < bModule && aModule < cModule) {
+            System.out.println(a);
+        } else {
+            if (bModule < cModule) {
+                System.out.println(b);
+            } else {
+                System.out.println(c);
+            }
+        }
+    }
+
     public static void main(String[] args){
         task1();
+        task2();
     }
 }
