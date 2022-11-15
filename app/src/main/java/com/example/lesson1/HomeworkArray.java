90079f09ea22e2322fa684cec7dfef7ad125783e
DanaKononova <dana.kononova@mail.ru>
2022-11-15 22:46:31 +0300

    commit taskArray1

--- app/src/main/java/com/example/lesson1/HomeworkArray.java
+++ app/src/main/java/com/example/lesson1/HomeworkArray.java
@@ -1,6 +1,16 @@
 package com.example.lesson1;
 
+import java.util.Arrays;
+
 public class HomeworkArray {
-    public static void main() {
+    public static void task1() {
+        String[] array = {"TViMS", "I", "do", "not", "love"};
+        System.out.println("Неотсортированный массив\n" + Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("Отсортированный массив\n" + Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        task1();
     }
 }
