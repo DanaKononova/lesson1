ef7c125eee700474b7ecbc5e48c9d0780d8c4bb1
DanaKononova <dana.kononova@mail.ru>
2022-11-15 23:28:12 +0300

    commit task2

--- app/src/main/java/com/example/lesson1/HomeworkArray.java
+++ app/src/main/java/com/example/lesson1/HomeworkArray.java
@@ -1,7 +1,47 @@
 package com.example.lesson1;
 
-public class HomeworkArray {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+public class HomeworkArray {
+    public static void task2() {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        boolean isSize = false;
+        int size;
+        do {
+            System.out.println("Введите размер массива");
+            size = scan.nextInt();
+            if (size > 5 && size <= 10) {
+                isSize = true;
+            }
+            else
+                System.out.println("Неверный размер");
+        } while (!isSize);
+
+        int size2 = 0;
+        int[] array1 = new int[size];
+        for (int i = 0; i < size; i++) {
+            array1[i] = random.nextInt(100);
+            if (array1[i] % 2 == 0) {
+                size2++;
+            }
+        }
+        System.out.println(Arrays.toString(array1));
+        int[] array2 = new int[size2];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (array1[i] % 2 == 0) {
+                array2[j++] = array1[i];
+            }
+        }
+        System.out.println("Массив из чётных чисел предыдущего массива:");
+        System.out.println(Arrays.toString(array2));
+
+    }
+
+    public static void main(String[] args) {
+        task2();
     }
 }
