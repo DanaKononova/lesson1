3a9c6ca1bbf5d92c890009a87f521d8c14926f64
DanaKononova <dana.kononova@mail.ru>
2022-11-15 23:39:55 +0300

    commit task3

--- app/src/main/java/com/example/lesson1/HomeworkArray.java
+++ app/src/main/java/com/example/lesson1/HomeworkArray.java
@@ -1,7 +1,40 @@
 package com.example.lesson1;
 
-public class HomeworkArray {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+public class HomeworkArray {
+    public static void task3() {
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[8];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        System.out.println("Массив чисел\n" + Arrays.toString(array));
+        System.out.println("Введите число, которое хотите удалить из массива");
+        int n = scan.nextInt();
+        boolean isInArray = false;
+        int size2 = 0;
+        for (int a: array) {
+            if (a == n) {
+                isInArray = true;
+            } else size2++;
+        }
+        if (isInArray) {
+            int[] array2 = new int[size2];
+            int j = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != n) {
+                    array2[j++] = array[i];
+                }
+            }
+            System.out.println("Массив без данного числа\n" + Arrays.toString(array2));
+        } else System.out.println("В массиве нет такого числа");
+    }
+
+    public static void main(String[] args) {
+        task3();
     }
 }
