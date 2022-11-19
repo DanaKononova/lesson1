4d15e7d094b809a4c2f1c8c7c17f22f80dce874e
DanaKononova <dana.kononova@mail.ru>
2022-11-19 23:55:38 +0300

    commit task2

--- app/src/main/java/com/example/lesson1/HomeworkArray2.java
+++ app/src/main/java/com/example/lesson1/HomeworkArray2.java
@@ -1,7 +1,36 @@
 package com.example.lesson1;
 
-public class HomeworkArray2 {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+public class HomeworkArray2 {
+    static Scanner scan = new Scanner(System.in);
+    static Random random = new Random();
+    static String[] numbers = {"one", "two","three","four","five","six","seven","eight","nine","ten"};
+
+    public static void task2() {
+        int[][][] threeDimensional;
+        String[][][] threeDimensionalWords;
+        System.out.println("Введите размерности трёхмерного массива:");
+        int size1 = scan.nextInt();
+        int size2 = scan.nextInt();
+        int size3 = scan.nextInt();
+        threeDimensional = new int[size1][size2][size3];
+        threeDimensionalWords = new String[size1][size2][size3];
+        for(int i = 0; i< size1; i++) {
+            for(int j = 0; j < size2; j++) {
+                for(int k = 0; k < size3; k++) {
+                    threeDimensional[i][j][k] = random.nextInt(10) + 1;
+                    threeDimensionalWords[i][j][k] = numbers[threeDimensional[i][j][k]-1];
+                }
+            }
+        }
+        System.out.println("Трёхмерный массив\n"+ Arrays.deepToString(threeDimensional));
+        System.out.println("Представление массива в виде слов:\n"+Arrays.deepToString(threeDimensionalWords));
+    }
+
+    public static void main(String[] args) {
+        task2();
     }
 }
