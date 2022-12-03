ed7dc93cd84b4b5a618a20c5cc1f56a79e4a269a
DanaKononova <dana.kononova@mail.ru>
2022-12-03 21:38:53 +0300

    commit homework fix

--- app/src/main/java/com/example/lessons/lesson9_Exceptions/Task1.java
+++ app/src/main/java/com/example/lessons/lesson9_Exceptions/Task1.java
@@ -3,24 +3,31 @@ package com.example.lessons.lesson9_Exceptions;
 import java.util.Scanner;
 
 public class Task1 {
-    public static boolean isCorrect(String word) {
-        int wordInt;
+    static final int WORD_LENGTH = 20;
+    static final int LOWER_START_ENGL = 65;
+    static final int LOWER_END_ENGL = 90;
+    static final int UPPER_START_ENGL = 97;
+    static final int UPPER_END_ENGL = 122;
+    static final int ZERO_CODE = 48;
+    static final int NINE_CODE = 57;
+
+    public static boolean isPasswordLoginCorrect(String word) {
         boolean isCorrect = true;
         for (int i = 0; i < word.length(); i++) {
-            wordInt = (int) word.charAt(i);
-            if (!(word.length() < 20 && ((wordInt >= 65 && wordInt <= 90)
-                    || (wordInt >= 97 && wordInt <= 122) || wordInt == '_'
-                    || (wordInt >= 48 && wordInt <= 57)))) {
+            int wordInt = (int) word.charAt(i);
+            if (!(word.length() < WORD_LENGTH && ((wordInt >= LOWER_START_ENGL && wordInt <= LOWER_END_ENGL)
+                    || (wordInt >= UPPER_START_ENGL && wordInt <= UPPER_END_ENGL) || wordInt == '_'
+                    || (wordInt >= ZERO_CODE && wordInt <= NINE_CODE)))) {
                 isCorrect = false;
             }
         }
-        return isCorrect;
+        return !isCorrect;
     }
 
-    public static boolean registration(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (!isCorrect(login)) throw new WrongLoginException("Неверный логин");
-        if (!isCorrect(password) || !isCorrect(confirmPassword) || (confirmPassword != password)) throw new WrongPasswordException("Неверный пароль");
-        return true;
+    public static void registration(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (isPasswordLoginCorrect(login)) throw new WrongLoginException("Неверный логин");
+        if (isPasswordLoginCorrect(password) || isPasswordLoginCorrect(confirmPassword) || (confirmPassword != password))
+            throw new WrongPasswordException("Неверный пароль");
     }
 
     public static void main(String[] args) {
@@ -32,16 +39,11 @@ public class Task1 {
         password = scanner.next();
         System.out.println("Подтвердите пароль");
         confirmPassword = scanner.next();
-        try{
+        try {
             registration(login, password, confirmPassword);
-        }
-        catch (WrongLoginException loginException) {
+        } catch (WrongLoginException | WrongPasswordException loginException) {
             loginException.printStackTrace();
-            System.out.println("Неверный логин");
-        }
-        catch (WrongPasswordException passwordException) {
-            passwordException.printStackTrace();
-            System.out.println("Неверный пароль");
+            loginException.getMessage();
         }
     }
 }
